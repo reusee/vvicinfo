fc52e84c66aef7e3cd5c48a129dbe0852d22e228
reus <reusee@gmail.com>
2016-08-31 02:05:00 +0800

    update

--- group.go
+++ group.go
@@ -1,53 +1,51 @@
 package main
 
 import (
-	"github.com/lib/pq"
 	"math"
 )
 
 func groupGoods() {
-	panic("TODO")
-	// load image and urls infos
-	hashToUrlIds := make(map[string][]int64)
-	//TODO 应该去掉小图片的，不过图片大小没有采集
-	rows, err := db.Query(`SELECT url_id, encode(sha512_16k, 'base64') FROM urls
+	goodIdToHashes := make(map[int32][]string)
+	hashToGoodIds := make(map[string][]int32)
+	rows, err := db.Query(`SELECT
+		good_id, encode(sha512_16k, 'base64')
+		FROM images
 		WHERE sha512_16k IS NOT NULL
-		--ORDER BY url_id DESC
-		--LIMIT 50000 -- DEBUG
-		`)
+		AND (CASE WHEN length > 0 THEN length >= 50000 ELSE true END)
+		`,
+	)
 	ce(err, "query")
+
 	n := 0
 	for rows.Next() {
-		var urlId int64
+		var goodId int32
 		var hash string
-		ce(rows.Scan(&urlId, &hash), "scan")
-		hashToUrlIds[hash] = append(hashToUrlIds[hash], urlId)
+		ce(rows.Scan(&goodId, &hash), "scan")
+		goodIdToHashes[goodId] = append(goodIdToHashes[goodId], hash)
+		hashToGoodIds[hash] = append(hashToGoodIds[hash], goodId)
 		n++
 		if n%10000 == 0 {
 			pt("%d\n", n)
 		}
 	}
 	ce(rows.Err(), "rows err")
-	pt("urls loaded\n")
+	pt("images data loaded\n")
 
-	urlIdToGoodIds := make(map[int64][]int64)
-	rows, err = db.Query(`SELECT good_id, url_id FROM images
-		--ORDER BY image_id DESC
-		--LIMIT 50000 -- DEBUG
-		`)
+	goodIds := make(map[int32]struct{})
+	rows, err = db.Query(`SELECT good_id
+		FROM goods
+		WHERE group_id IS NULL
+		AND images_collected = true
+		`,
+	)
 	ce(err, "query")
-	n = 0
 	for rows.Next() {
-		var goodId, urlId int64
-		ce(rows.Scan(&goodId, &urlId), "scan")
-		urlIdToGoodIds[urlId] = append(urlIdToGoodIds[urlId], goodId)
-		n++
-		if n%10000 == 0 {
-			pt("%d\n", n)
-		}
+		var goodId int32
+		ce(rows.Scan(&goodId), "scan")
+		goodIds[goodId] = struct{}{}
 	}
 	ce(rows.Err(), "rows err")
-	pt("images loaded\n")
+	pt("good ids loaded\n")
 
 	txCount := 0
 	tx := db.MustBegin()
@@ -55,47 +53,27 @@ func groupGoods() {
 
 check:
 
-	// get good id
-	var goodId int64
-	ce(tx.Get(&goodId, `SELECT 
-		good_id
-		FROM goods
-		WHERE group_id IS NULL 
-		AND images_collected = true
-		ORDER BY good_id DESC
-		LIMIT 1`), "get good id")
-
-	// get good hashes
-	var hs pq.StringArray
-	ce(tx.Select(&hs, `SELECT
-		encode(sha512_16k, 'base64')
-		FROM images i
-		LEFT JOIN urls USING(url_id)
-		WHERE
-		i.good_id = $1
-		`,
-		goodId,
-	), "select hashes")
-	hashes := make(map[string]struct{})
-	for _, h := range hs {
-		hashes[h] = struct{}{}
+	var goodId int32
+	for id := range goodIds {
+		goodId = id
+		break
 	}
+	hashes := goodIdToHashes[goodId]
 
-	// stat
-	matches := make(map[int64]map[string]struct{})
-	for hash := range hashes {
-		urlIds := hashToUrlIds[hash]
-		for _, urlId := range urlIds {
-			for _, rightId := range urlIdToGoodIds[urlId] {
-				if _, ok := matches[rightId]; !ok {
-					matches[rightId] = make(map[string]struct{})
-				}
-				matches[rightId][hash] = struct{}{}
+	matches := make(map[int32]map[string]struct{})
+	for _, hash := range hashes {
+		rightIds := hashToGoodIds[hash]
+		for _, rightId := range rightIds {
+			if _, ok := matches[rightId]; !ok {
+				matches[rightId] = make(map[string]struct{})
 			}
+			matches[rightId][hash] = struct{}{}
 		}
 	}
+
 	has := false
 	for rightId, hashSet := range matches {
+		//TODO 去掉第二个条件
 		if len(hashSet) >= 10 && math.Abs(float64(len(hashes)-len(hashSet))) < 5 {
 			pt("%d %d %d %d - \n", goodId, len(hashes), rightId, len(hashSet), markedCount)
 			// 不到的话就算了吧
