0a024bba9aa9af3427aaef0692f787a0f44f628f
reus <reusee@gmail.com>
2016-09-17 18:10:23 +0800

    collect description and attributes

--- collect_goods.go
+++ collect_goods.go
@@ -21,7 +21,7 @@ func collectGoods() {
 do:
 	pt("select good ids to fetch images\n")
 	var ids, filtered []int64
-	err := db.Select(&ids, `SELECT good_id FROM images_not_collected`)
+	err := db.Select(&ids, `SELECT good_id FROM images_not_collected ORDER BY good_id DESC`)
 	ce(err, "select ids")
 	for _, id := range ids {
 		if zeroImagesIds[id] > 5 {
@@ -81,7 +81,8 @@ get:
 			Status int    // 上下架状态
 			Size   string // 尺码
 			Color  string // 颜色
-			Is_tx  int
+			Is_tx  int    // 退现
+			Attrs  string // 属性
 		}
 	}
 	err = json.NewDecoder(resp.Body).Decode(&data)
@@ -92,62 +93,67 @@ get:
 		}
 		ce(err, "decode json %s", pagePath)
 	}
-	ce(withTx(db, func(tx *sqlx.Tx) (err error) {
-		defer ct(&err)
-		nImages := 0
-		for _, imgPath := range strings.Split(data.Data.Imgs, ",") {
-			if imgPath == "" {
-				continue
-			}
-			if !strings.HasPrefix(imgPath, "http:") {
-				imgPath = "http:" + imgPath
-			}
-			ce(saveGoodImage(tx, id, imgPath), "save image url")
-			n++
-			nImages++
+
+	tx := db.MustBegin()
+
+	nImages := 0
+	for _, imgPath := range strings.Split(data.Data.Imgs, ",") {
+		if imgPath == "" {
+			continue
 		}
-		doc, err := goquery.NewDocumentFromReader(strings.NewReader(data.Data.Desc))
-		ce(err, "goquery doc")
-		doc.Find("img").Each(func(i int, se *goquery.Selection) {
-			imgSrc, _ := se.Attr("src")
-			if !strings.HasPrefix(imgSrc, "http") {
-				return
-			}
-			ce(saveGoodImage(tx, id, imgSrc), "save image url")
-			n++
-			nImages++
-		})
-		_, err = tx.Exec(`UPDATE goods 
-			SET status = $1, sizes = $2, colors = $3, tuixian = $5
+		if !strings.HasPrefix(imgPath, "http:") {
+			imgPath = "http:" + imgPath
+		}
+		ce(saveGoodImage(tx, id, imgPath), "save image url")
+		n++
+		nImages++
+	}
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(data.Data.Desc))
+	ce(err, "goquery doc")
+	doc.Find("img").Each(func(i int, se *goquery.Selection) {
+		imgSrc, _ := se.Attr("src")
+		if !strings.HasPrefix(imgSrc, "http") {
+			return
+		}
+		ce(saveGoodImage(tx, id, imgSrc), "save image url")
+		n++
+		nImages++
+	})
+	_, err = tx.Exec(`UPDATE goods 
+			SET status = $1, sizes = $2, colors = $3, tuixian = $5, attributes = $6, description = $7
 			WHERE good_id = $4`,
-			data.Data.Status,
-			data.Data.Size,
-			data.Data.Color,
-			id,
-			data.Data.Is_tx,
-		)
-		ce(err, "update status")
-		if nImages > 0 {
-			_, err = tx.Exec(`DELETE FROM images_not_collected
+		data.Data.Status,
+		data.Data.Size,
+		data.Data.Color,
+		id,
+		data.Data.Is_tx,
+
+		data.Data.Attrs,
+		data.Data.Desc,
+	)
+	ce(err, "update status")
+	if nImages > 0 {
+		_, err = tx.Exec(`DELETE FROM images_not_collected
 				WHERE good_id = $1
 				`,
-				id,
-			)
-			ce(err, "delete images_not_collected entry")
-			_, err = tx.Exec(`UPDATE goods
+			id,
+		)
+		ce(err, "delete images_not_collected entry")
+		_, err = tx.Exec(`UPDATE goods
 				SET images_collected = true
 				WHERE good_id = $1
 				`,
-				id,
-			)
-			ce(err, "update goods.images_collected")
-		} else {
-			zeroImagesIdsLock.Lock()
-			zeroImagesIds[id]++
-			zeroImagesIdsLock.Unlock()
-		}
-		return
-	}), "tx")
+			id,
+		)
+		ce(err, "update goods.images_collected")
+	} else {
+		zeroImagesIdsLock.Lock()
+		zeroImagesIds[id]++
+		zeroImagesIdsLock.Unlock()
+	}
+
+	ce(tx.Commit(), "commit")
+
 	return
 }
 
@@ -173,6 +179,7 @@ func saveGoodImage(tx *sqlx.Tx, goodId int64, url string) (err error) {
 	_, err = tx.Exec(`INSERT INTO image_vars
 		(image_id)
 		VALUES ($1)
+		ON CONFLICT (image_id) DO NOTHING
 		`,
 		imageId,
 	)
